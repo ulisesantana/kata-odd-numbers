09ec38d68a37d28927535bd1e83c97349d0f8f1f
Ulises Santana <ulisesantana@gmail.com>
2021-01-29 12:49:35 +0000

    first implementation

--- src/green.test.ts
+++ src/green.test.ts
@@ -1,5 +1,23 @@
-describe('Basic tests', () => {
-  it('Always green', () => {
-    expect(true).toBe(true)
-  })
+function odd(input: number[]): number[] {
+    return [...new Set<number>(
+        input.map(x => [
+            x,
+            input.reduce((repeatedTimes, y) => x === y
+                ? repeatedTimes + 1
+                : repeatedTimes, 0)]
+        )
+            .filter(([, repeatedTimes]) => repeatedTimes % 2 !== 0)
+            .map(([num]) => num)
+    )]
+}
+
+describe('Odd function should', () => {
+    it('return an array', () => {
+        expect(odd([])).toStrictEqual([])
+    })
+    it('return the numbers that are repeated odd times', () => {
+        expect(odd([2])).toStrictEqual([2])
+        expect(odd([2, 1, 1])).toStrictEqual([2])
+        expect(odd([2, 1, 1, 1])).toStrictEqual([2, 1])
+    })
 })
