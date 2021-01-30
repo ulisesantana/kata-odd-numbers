65da0e45bff285a6d4d9a57eb87443a45f267777
Ulises Santana <ulisesantana@gmail.com>
2021-01-30 01:52:32 +0000

    implement recursive solution

--- src/extractNumbersRepeatedOddTimes.test.ts
+++ src/extractNumbersRepeatedOddTimes.test.ts
@@ -7,7 +7,9 @@ describe('extractNumbersRepeatedOddTimes should', () => {
   it.each([
     [[2], [2]],
     [[2, 1, 1], [2]],
-    [[2, 1, 1, 1], [2, 1]]
+    [[2, 1, 1, 1], [1, 2]],
+    [[1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5], [1, 3, 5]],
+    [[2, 12, 2, 2, 3, 4, 3, 2, 5, 4, 3, 5, 56, 4, 3, 2, 1], [1, 2, 4, 12, 56]]
   ])('return the numbers that are repeated odd times. For %j should return %j', (input, expected) => {
     expect(extractNumbersRepeatedOddTimes(input)).toStrictEqual(expected)
   })
--- src/extractNumbersRepeatedOddTimes.ts
+++ src/extractNumbersRepeatedOddTimes.ts
@@ -1,36 +1,57 @@
 export const extractNumbersRepeatedOddTimes = pipe(
-  computeRepeatedTimes,
-  filterNumbersRepeatedEvenTimes,
-  mapToNumber,
-  removeRepeatedNumbers
+  sortList,
+  groupNumbers,
+  filterNumbersRepeatedEvenTimes
 )
 
 function pipe (...process: Function[]): (input: number[]) => number[] {
-  return (input: number[]) => process.reduce((result, fn) => fn(result), input)
+  return (input: number[]) => {
+    if (process.length === 0) {
+      return input
+    }
+    const [nextProcess, ...processQueue] = process
+    return pipe(...processQueue)(nextProcess(input))
+  }
 }
 
-function computeRepeatedTimes (list: number[]): Array<[number, number]> {
-  return list.map(number => [number, reduceRepeatedTimes(list, number)])
+function sortList (list: number[]): number[] {
+  return list.sort((a, b) => a > b
+    ? 1
+    : a < b
+      ? -1
+      : 0
+  )
 }
 
-function reduceRepeatedTimes (list: number[], repeatedNumber: number): number {
-  return list.reduce((repeatedTimes, number) =>
-    repeatedNumber === number
-      ? repeatedTimes + 1
-      : repeatedTimes,
-  0)
+function groupNumbers (listToCheck: number[], groupedNumbers = [] as number[][]): number[][] {
+  const [numberToGroup] = listToCheck
+  const nextNumberToGroupIndex = listToCheck.lastIndexOf(numberToGroup) + 1
+
+  if (nextNumberToGroupIndex === listToCheck.length) {
+    return [...groupedNumbers, listToCheck]
+  }
+
+  if (nextNumberToGroupIndex < listToCheck.length) {
+    return groupNumbers(
+      listToCheck.slice(nextNumberToGroupIndex),
+      [...groupedNumbers, Array<number>(nextNumberToGroupIndex).fill(numberToGroup)]
+    )
+  }
+
+  return groupedNumbers
 }
 
-function filterNumbersRepeatedEvenTimes (list: Array<[number, number]>): Array<[number, number]> {
-  return list.filter(([_, repeatedTimes]) => repeatedTimes % 2 !== 0)
-}
-
-function mapToNumber (list: Array<[number, number]>): number[] {
-  return list.map(([number]) => number)
-}
-
-function removeRepeatedNumbers (list: number[]): number[] {
-  return [...new Set(list)]
+function filterNumbersRepeatedEvenTimes (listToCheck: number[][], filteredNumbers = [] as number[]): number[] {
+  const [head, ...tail] = listToCheck
+  if (head === undefined) {
+    return filteredNumbers
+  }
+  return filterNumbersRepeatedEvenTimes(
+    tail,
+    head.length % 2 !== 0
+      ? [...filteredNumbers, head[0]]
+      : filteredNumbers
+  )
 }
 
 export default extractNumbersRepeatedOddTimes
